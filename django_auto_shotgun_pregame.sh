c011e778b700f8ea00dff7d00e6b328871eb78c8
TheLurkiest <bng5@wildcats.unh.edu>
2019-05-22 16:42:57 -0400

    reintegrated our while loop back into our original shell script now that it is working again

--- django_auto_shotgun_pregame.sh
+++ django_auto_shotgun_pregame.sh
@@ -154,10 +154,10 @@ echo "now cd into the api_auto_xxx folder you created and execute the shell scri
 
 
 
-
-
 sudo rm -r field_and_model_info
 mkdir field_and_model_info
+
+outer_folder='field_and_model_info/'
 
 echo 'how many models do you want to make to start out with?'
 read num_models
@@ -168,14 +168,17 @@ read model_name
 echo 'how many fields do you want to make for this model?'
 read num_fields
 
-model_folder_now='field_and_model_info/'$model_name
-mkdir $model_folder_now
+model_now_var=$outer_folder$model_name
+
+touch $model_now_var
+
 
 while :
 do
 	if [ $num_fields -lt 1 ]
 	then
-		num_models=$[ $num_models - 1 ]
+		num_models=$[ $num_models -1 ]
+		
 		if [ $num_models -lt 1 ]
 		then
 			echo 'all fields and all models have been created!!!  Moving on...'
@@ -185,19 +188,20 @@ do
 			read model_name
 			echo 'how many fields do you want to make for that model?'
 			read num_fields
-			model_folder_now='field_and_model_info/'$model_name
-			mkdir model_folder_now
+			model_now_var=$outer_folder$model_name
+			touch $model_now_var
 		fi
 	else
 		echo 'what would you like this field to be named?'
 		read field_name
-		echo $field_name >> model_folder_now
-		num_models=$[ $num_fields - 1 ]
+		echo $field_name >> $model_now_var
+		num_fields=$[ $num_fields -1 ]
 	fi
 	echo "models remaining to set up: "$num_models
 	echo "fields remaining to set up: "$num_fields
 
 done
+
 
 
 
