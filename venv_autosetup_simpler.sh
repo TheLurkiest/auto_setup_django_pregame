74930b3408bc5641b16f3ff2fde174625a22970b
TheLurkiest <fanningb142@gmail.com>
2019-07-17 05:03:02 +0000

    we created an automatic virtual environment creation system for an even quicker setup-- starting from absolutely nothing to a fully fledged API

---
+++ venv_autosetup_simpler.sh
@@ -0,0 +1,22 @@
+# to shorten it just execute this shell script to set up the virtual environment automatically:
+sudo apt-get install python3-venv
+python3 -m venv environV77
+source environV77/bin/activate
+pip3 install django
+pip install djangorestframework
+
+echo -------------------------------------------------------------------------------
+
+echo ok, the virtual environment you just auto set up is now ready to go:
+
+
+
+echo to get started using it, you must activate the virtual environment using the following terminal command: source environV77/bin/activate
+
+echo ...after that just execute the main shell script using the following terminal command: ./main_auto_api_maker2.sh
+
+echo --then hit enter at the first prompt
+
+echo --and just follow the instructions that are displayed on the terminal to complete the process!
+
+echo --this should take less than 30 seconds.
