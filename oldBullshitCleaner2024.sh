69dda67b6eea4c719b9a41d8bda5b2d58d5c17e0
TheLurkiest <brent.fanning@wildcats.unh.edu>
2024-03-21 20:49:55 -0400

    made small changes in order to clean up old clutter a little bit better whenever we test out our auto-django-setup process in order to remove some of the BS

---
+++ oldBullshitCleaner2024.sh
@@ -0,0 +1,20 @@
+echo "execute the following shell script to remove the extra clutter generated as a byproduct of our quick and dirty auto-django-setup process testing, so that we can more easily locate, isolate and identify the actual changes we made that potentially gave us the results we got in order to correctly add ONLY those changes to our existing github repo that RESULTED in the changes we saw from testing out the auto-django-setup, rather than cluttering up our github repo by accidentally adding the results themselves."
+
+echo "-------------------------------------------"
+echo " "
+
+sudo rm -r lc_mf[1-9]
+sudo rm -r m1_file_tests[1-9]
+sudo rm -r dir[1-9]
+sudo rm -r dir_b[1-9]
+sudo rm -r a2_pt2_file
+sudo rm -r dir_b0
+sudo rm -r dir0
+sudo rm -r f1_file
+sudo rm -r stage1_auto_d.py
+sudo rm -r testing_test_maker.py
+sudo rm -r type_application_clean_slate_final.py
+sudo rm -r seven_namesakes
+sudo rm -r m1_file
+sudo rm -r m1_file_tests
+sudo rm -r api_auto_xxx
